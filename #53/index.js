af2da4c0094cd54b3783893d7a69052f4dba83a1
Syed Harrum <syedharrumten@gmail.com>
2026-04-27 15:43:36 +0500

    stack & queue - #53

---
+++ #53/index.js
@@ -0,0 +1,98 @@
+// stack dsa 
+
+// problem 1 
+
+// Stack
+
+// let stack = [];
+
+// function push(value) { 
+//     stack.push(value)
+// }
+// function pop() { 
+//     stack.pop()
+// }
+// function peek() { 
+//     stack.peek(2)
+// }
+
+// push("harrum")
+// console.log(stack);
+
+// pop()
+
+// console.log(stack);
+
+// peek()
+
+// console.log(stack);
+
+// Queue 
+
+// problem 2 
+
+// let queue = [];
+
+// function enqueue(value) { 
+//     queue.push(value); 
+// }
+
+
+// function dequeue() { 
+//     return queue.shift(); 
+// }
+
+// function peek() { 
+//     return queue[0];
+// }
+
+// enqueue(1); 
+// enqueue(2); 
+// enqueue(3); 
+
+// console.log(dequeue()); 
+// console.log(peek());    
+
+// problem 3 
+
+// let stack = [];
+
+// function reverseString(str) {
+//     for(let i = 0; i < stack.length; i++) {
+//         stack.push(str[i])
+//     }
+//     let reverseStr = "";
+
+//     while (stack.length > 0) {
+//         reversedStr += stack.pop();
+//     }
+
+//     return reversedStr;
+// }
+
+// console.log(reverseString("hello"))
+
+// problem 4 
+
+// let stack = [];
+
+// function isBalanced(str) {
+//     for(let i = 0; i < stack.length; i++) {
+//         if(str[i] === "({[]})") {
+//             return stack.push("true")
+//         }else {
+//             return stack.push("false")
+//         }
+//     }
+// }
+
+// console.log(isBalanced("({[]})"))  
+// console.log(isBalanced("({[)}")) 
+
+// problem 5 
+
+// let queue = [];
+
+// for(let i = 0; i < 5; i++) {
+//     return queue.unshift(i)
+// }

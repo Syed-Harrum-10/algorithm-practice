618644a82723ee6bee9339970deaf7545203107e
Syed Harrum <syedharrumten@gmail.com>
2026-06-05 13:04:00 +0500

    new code

---
+++ #73/index.js
@@ -0,0 +1,55 @@
+// #1 
+
+// function factor (n) {
+//    if(n <= 1) {
+//     return 1;
+//    }
+//    return n * factor(n - 1)
+// }
+
+
+// console.log(factor(5));
+
+// #2
+
+// function fib (n) {
+//     if(n === 0) return 0;
+//     if(n === 1) return 1;
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// console.log(fib(6));
+
+// #3
+
+// function sum (n) {
+//     if(n === 1) return 1;
+
+//     return n + sum(n - 1)
+// }
+
+// console.log(sum(5));
+
+// #4
+
+// function power(n, count) {
+
+//     if (count === 0) return 1;
+
+//     return n * power(n, count - 1);
+// }
+
+// console.log(power(2, 5)); 
+
+// #5
+
+// function reverse(str) {
+
+//     if (str === "") return "";
+
+//     return str[str.length - 1] + reverse(str.slice(0, -1))
+// }
+
+// console.log(reverse("hello")); 
+

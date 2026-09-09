28a8b35466d9760cbd972cdb8baee668ce1eced8
Syed harrum <syedharrumten@gmail.com>
2026-09-09 14:49:29 +0500

    Time: 0 ms (100.00%) | Memory: 54.2 MB (11.96%) - LeetSync

---
+++ 198-house-robber/house-robber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    let dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for(let i = 2; i < dp.length; i++) {
+        dp[i] = Math.max(nums[i] + dp[i-2], dp[i - 1]);
+    }
+
+    return dp[nums.length - 1]
+
+}

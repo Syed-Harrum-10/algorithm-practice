8d0ecdb63f123251e4bf0c518db268f192a290a5
Syed harrum <syedharrumten@gmail.com>
2026-09-23 13:22:06 +0500

    Time: 31 ms (90.17%), Space: 67.8 MB (91.47%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function kClosest (points, k) {
+    let sort = points.sort((a, b) => (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]))
+    return sort.slice(0, k);
+}
